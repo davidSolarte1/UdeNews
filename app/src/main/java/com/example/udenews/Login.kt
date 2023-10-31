1d8e840f00f22f546a121cb44af6f35a9a34aaf8
davidSolarte1 <dsolarte2809@gmail.com>
2023-10-30 22:50:26 -0500

    Ingresar con correo y contraseña mediante firebase

--- app/src/main/java/com/example/udenews/Login.kt
+++ app/src/main/java/com/example/udenews/Login.kt
@@ -3,21 +3,67 @@ package com.example.udenews
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import com.example.udenews.databinding.ActivityLoginBinding
+import com.example.udenews.databinding.ActivitySignInBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+        auth = Firebase.auth
 
-        btnSignIn.setOnClickListener {
-
-            startActivity(Intent(this, SignIn::class.java))
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etUser.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(baseContext, "Complete todos los campos", Toast.LENGTH_SHORT,).show()
+            }
+            else{
+                LogIn(email, password)
+            }
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload()
+        }
+    }
+    private fun LogIn(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful) {
+                Log.d("TAG", "signInWithEmail:success")
+                reload()
+
+            } else {
+                Log.w("TAG", "signInWithEmail:failure", task.exception)
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed.", Toast.LENGTH_SHORT,
+                ).show()
+
+            }
+        }
+
+    }
+    private fun reload(){
+        val intent = Intent(this, Main::class.java)
+        this.startActivity(intent)
     }
 }
