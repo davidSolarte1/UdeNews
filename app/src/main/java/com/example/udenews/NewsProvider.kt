320103d9e074df62a24aebec8adf2e0ce5e1752d
BrayanPiandoy <91925316+BrayanPiandoy@users.noreply.github.com>
2023-11-04 08:01:48 -0500

    main

---
+++ app/src/main/java/com/example/udenews/NewsProvider.kt
@@ -0,0 +1,31 @@
+package com.example.udenews
+
+class NewsProvider {
+
+    val newslist = listOf<News>(
+        News(
+            "fiestas_ing",
+            "semana para festejar",
+            "https://periodico.udenar.edu.co/wp-content/uploads/2016/11/universidad-de-narino-edificio-de-ingieneria-udenar-periodico.jpg"
+        ),
+
+        News(
+            "fiestas_ing",
+            "semana para festejar",
+            "https://periodico.udenar.edu.co/wp-content/uploads/2016/11/universidad-de-narino-edificio-de-ingieneria-udenar-periodico.jpg"
+        ),
+
+        News(
+            "fiestas_ing",
+            "semana para festejar",
+            "https://periodico.udenar.edu.co/wp-content/uploads/2016/11/universidad-de-narino-edificio-de-ingieneria-udenar-periodico.jpg"
+        ),
+
+        News(
+            "fiestas_ing",
+            "semana para festejar",
+            "https://periodico.udenar.edu.co/wp-content/uploads/2016/11/universidad-de-narino-edificio-de-ingieneria-udenar-periodico.jpg"
+        )
+
+    )
+}
